1b1f8ab6854494bca8a5b85dff86312f63d254c3
Ivan Stana <stiipa@centrum.sk>
2012-03-23 21:33:13 +0100

    load configuration file only once for instance

--- lib/core.rb
+++ lib/core.rb
@@ -1,8 +1,14 @@
 require 'yaml'
 
-module AAcore
+class AAcore
+  attr_reader :config
+  
+  def initialize
+    @config = load_config
+  end 
+  
   def dbstring(usertype='dbreader')
-    db = load_config(usertype)
+    db = process_config(usertype)
     
     if db['username'].empty?
         loginseq = ''
@@ -11,26 +17,27 @@ module AAcore
     elsif !db['username'].empty? && !db['password'].empty?
         loginseq = username+':'+password+'@'
     end
-    database = db['protocol']+'://'+loginseq+db['host']+':'+db['port']+'/'+db['db']+'/'
-    database
+    
+    db['protocol']+'://'+loginseq+db['host']+':'+db['port']+'/'+db['db']+'/'
   end
 
-  def load_config(usertype='dbreader')
+  def load_config
     # load database
     file ||= File.join(Rails.root,'config/couchdb.yml') if (defined? Rails) && File.exists?(File.join(Rails.root,'config/couchdb.yml'))
     file ||= File.join(File.dirname(__FILE__),'config/couchdb.yml') if File.exists?(File.join(File.dirname(__FILE__),'config/couchdb.yml'))
     file ||= 'couchdb.yml' if File.exists?('couchdb.yml')
     
+    @config = ::YAML::load(File.open(file)) unless file==nil  
+  end
+  
+  def process_config(usertype='dbreader')
     env = ENV['RACK_ENV'] || 'development'
     env = Rails.env if defined? Rails
-    # DEBUG
-    puts file
-    config = ::YAML::load(File.open(file)) unless file==nil
-
-    if config==nil || !config.include?(usertype) || !config[usertype].include?(env)
+  
+    if @config==nil || !@config.include?(usertype) || !@config[usertype].include?(env)
       databaseuri = {'protocol'=>'http', 'username'=>'', 'password'=>'', 'host'=>'localhost', 'port'=>'5984', 'db'=>'sa'}
     else
-      par = config[usertype][env]
+      par = @config[usertype][env]
       protocol = par['protocol'] || 'http'
       host = par['host'] || 'localhost'
       port = par['port'].to_s || '5984'
