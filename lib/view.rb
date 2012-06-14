87eb91dd8bcb7beb6f91754f8adcf3f48b9dbe64
Ivan Stana <stiipa@centrum.sk>
2012-06-14 22:44:27 +0200

    View functionality born from mess in Document and DocumentApi

---
+++ lib/view.rb
@@ -0,0 +1,25 @@
+class Divan::View
+  include HTTParty
+  
+  
+# TODO create methods returning raw result(s)?  
+# design doc, view, optional - key/startkey+endkey, descending (true), group (true), include_docs (true)
+=begin
+key
+keys
+startkey
+startkey_docid
+endkey
+endkey_docid
+limit
+stale
+descending
+skip
+group
+group_level
+reduce
+include_docs
+inclusive_end
+update_seq 
+=end
+end 
