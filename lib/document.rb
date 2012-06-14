407ad90d4065a339f92a60f93bb2dc197c1735fe
Ivan Stana <stiipa@centrum.sk>
2012-06-14 22:42:53 +0200

    DocumentApi functionality merged to Document

--- lib/document.rb
+++ lib/document.rb
@@ -20,7 +20,28 @@ class Document
   # better beahaviour with no trailing /
   base_uri 'http://localhost:5984/ionorchis_development'
             #Divan::Configuration.dbstring
-  #default_params :output => 'json'
+            
+  
+  
+  
+  
+  # special fields
+  # _id - unique identifier (mandatory and immutable)
+  # _rev - mvcc revision  (mandatory and immutable)
+  # _attachments - attachments metadata
+  # _deleted has been deleted?
+  # And previous revisions will be removed on next compaction run
+  # _revisions - revision history of the document
+  # _revs_info - list of revisions of the document, and their availability
+  # _conflicts - information about conflicts
+  # _deleted_conflicts - information about conflicts
+  # _local_seq - sequence number of the revision in the database (as found in the _changes feed) 
+  # curl -X GET 'http://localhost:5984/my_database/my_document?conflicts=true'
+  # The query parameter for the _revisions special field is revs
+  #
+  # this adds some overhead TODO only in size of JSON or also compute power?
+  # if false or empty, it isn't present in document
+  default_params :deleted => true, :attachments => true, :conflicts => true
   
   # new record, which is not in database
   # random unique ID is assigned, if not given
@@ -71,7 +92,6 @@ class Document
   end
 
 
-
   def conflict?
     @confl
   end
@@ -80,18 +100,25 @@ class Document
   def new?
     !instance_variable_defined? '@_rev'.to_sym
   end
+  
+  
+  def deleted?
+    self.respond_to? :_deleted
+  end
 
   
   def save
-    result = self.class.put('/'+id, :body => self.to_s)
+    doc = self.document
+    doc.delete('_id')
+    @last_request = self.class.put('/'+@_id, :body => doc)
     
-    if result.conflict?
+    if @last_request.conflict?
       @confl = true
-      return false
+      raise("Conflict", "Document conflict!")
     end
     
     if result.success? && result.body['ok']==true
-      instance_variable_set rev.body['rev']
+      instance_variable_set rev.body['_rev']
     else
       return false
     end
@@ -110,7 +137,8 @@ class Document
     f = f.to_s
     unless f.empty? || instance_variable_defined?("@#{f}")
       self.instance_variable_set "@#{f}", val
-      alias_method "alias_#{f}", f if self.respond_to? f.to_sym
+      # probably not
+      #alias_method "alias_#{f}", f if self.respond_to? f.to_sym
       # accessor
       self.define_singleton_method f do
         instance_variable_get "@#{f}".to_sym
@@ -120,11 +148,11 @@ class Document
         instance_variable_set "@#{f}".to_sym, param1
       end
     else
-      #raise("ArgumentError", "field name cannot be nil or this field exists already")
+      raise("ArgumentError", "field name cannot be nil or this field exists already")
     end
     @mapped_fields << f.to_s
   end
-=begin  
+  
   def remove_field(f)
     f = f.to_s
     unless f.empty?
@@ -133,27 +161,27 @@ class Document
         remove_method f.to_sym
       #end
       remove_instance_variable "@#{f}".to_sym
-      
-      if self.respond_to? "alias_#{f}".to_sym
-        alias_method f, "alias_#{f}"
-        remove_method "alias_#{f}".to_sym
-      end
     end
   end
-=end
-
   
+
+  # options - see special fields in default_params
+  # another options is :all => whatever, which fetches all (available) special fields
   def self.fetch(id, options = {})
-    #TODO specialized fields
-    d = self.get('/'+id)
+    if options.has_key? :all
+      options = {:revs => true, :revs_info => true, :deleted_conflicts => true, :local_seq => true}
+    end
+    
+    d = self.class.get('/'+id, :query => options)
     if d.success?
       Document.new(d)
     else
       raise("DivanDocumentRetrievalError", "Document could not be fetched from CouchDB")
     end
   end
-
-  def to_s
+  
+  
+  def document
     doc = {}
     @mapped_fields.each do |field|
       # to have more levels of hash correct
@@ -162,81 +190,17 @@ class Document
     end
     doc
   end
-end
-
-
-
-
-
-
-
-
-
-=begin
-class Divan::Document
-  include ::Divan::Common
-  attr_accessor :database, :type, :doc, :messages
-
+  
   def design
     self.class.to_s
   end
   
-  #hash todo class method
-  def dgetraw(id)
-    @doc = rawget(@database+id)
+  def attachment_put
+  
   end
+  
+  def attachment_delete
+  
+  end
+end
 
-  # todo class method, todo _all_docs, todo 404 error
-  # probably array of Documents, design doc name is calculated
-  def dsget(view, options={})
-    resultraw = rawget(@database+'_/design/'+design+'/_view/'+view)
-    result = resultraw['result']['rows']
-    docs = []
-    result['rows'].each do |doc|
-      docs << Document.new('doc' => result['rows'])
-    end
-    
-    metadata = resultraw['result']
-    metadata.delete('rows')
-    metadata.merge(resultraw['headers'])
-    
-    docs.define_singleton_method :metadata, lambda { metadata }
-    docs 
-  end
-  
-  # only hash
-  def dsgetraw(view, options={})
-    resultraw = rawget(@database+'_/design/'+design+'/_view/'+view)
-    docs = resultraw['result']['rows']
-    
-    metadata = resultraw['result']
-    metadata.delete('rows')
-    metadata.merge(resultraw['headers'])
-    
-    docs.define_singleton_method :metadata, lambda { metadata }
-    docs
-  end
-  
-  # design doc (via @type), view, optional - key/startkey+endkey, descending (true), group (true), include_docs (true)
-=end
-=begin
-  def dget(view, options = {})
-key
-keys
-startkey
-startkey_docid
-endkey
-endkey_docid
-limit
-stale
-descending
-skip
-group
-group_level
-reduce
-include_docs
-inclusive_end
-update_seq 
-    
-  end
-=end 
