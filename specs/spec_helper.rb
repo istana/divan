fe3193c5328376c3fd4a1740bd34b5cb8deffba8
Ivan Stana <stiipa@centrum.sk>
2012-07-22 22:32:00 +0200

    new shape of spec helper

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -3,7 +3,13 @@ require "rubygems"
 require "rspec"
 #require "ruby-debug19"
 
+ENV['RACK_ENV']='test'
 require File.join(File.dirname(__FILE__), '..','lib','divan.rb')
+Divan::Configuration.load_config(File.join(File.dirname(__FILE__), '..','lib','couchdb.yaml'))
+# Replenish configuration for parts, that uses HTTParty
+Divan::Document.base_uri Divan::Configuration.dbstring
+Divan::DBAdmin.base_uri Divan::Configuration.dbstring('dbadmin')
+#Divan::Design.base_uri Divan::Configuration.dbstring
 #require File.join(File.dirname(__FILE__), '..','lib','design.rb')
 
 
