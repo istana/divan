31b71382f6c8db777cd3af3d813919b5631e759d
Ivan Stana <stiipa@centrum.sk>
2012-07-28 23:55:51 +0200

    Parentheses were missing :-(

--- lib/common.rb
+++ lib/common.rb
@@ -48,8 +48,8 @@ class Object
   def blank?
     return true if self.nil?
     return true if self == false
-    return true if self.respond_to? :empty? && self.empty?
-    return true if self.is_a? String && /\A\s*\z/.match(self)
+    return true if self.respond_to?(:empty?) && self.empty?
+    return true if self.is_a?(String) && /\A\s*\z/.match(self)
     false 
   end
 end
